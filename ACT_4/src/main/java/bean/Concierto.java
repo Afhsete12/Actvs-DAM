58941bf00d7d483d054b5d11cfa343c33c5215ad
Afhsete12 <jadripes2020@gmail.com>
2022-02-20 15:29:24 +0100

    Update package bean

--- ACT_4/src/main/java/bean/Concierto.java
+++ ACT_4/src/main/java/bean/Concierto.java
@@ -1,5 +1,52 @@
 package bean;
 
-public class Concierto {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+@XmlRootElement(name="concierto")
+public class Concierto {
+	private String fecha;
+	private String hora;
+	private List<Participante> participantes;
+	
+	public Concierto() {
+		super();
+	}
+	
+	public Concierto(String fecha, String hora) {
+		super();
+		this.fecha = fecha;
+		this.hora = hora;
+		participantes = new ArrayList<Participante>();
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getHora() {
+		return hora;
+	}
+
+	public void setHora(String hora) {
+		this.hora = hora;
+	}
+	@XmlElement(name = "participante")
+	@XmlElementWrapper(name = "participantes")
+	public List<Participante> getParticipantes() {
+		return participantes;
+	}
+
+	public void setParticipantes(List<Participante> participantes) {
+		this.participantes = participantes;
+	}
+	
+	
 }
--- ACT_4/src/main/java/bean/Participante.java
+++ ACT_4/src/main/java/bean/Participante.java
@@ -1,5 +1,37 @@
 package bean;
 
-public class Participante {
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
+@XmlRootElement(name = "participante")
+public class Participante {
+	private String h_entrada;
+	private String nombre;
+	
+	public Participante() {
+		super();
+	}
+
+	public Participante(String h_entrada, String nombre) {
+		super();
+		this.h_entrada = h_entrada;
+		this.nombre = nombre;
+	}
+	@XmlElement(name="entrada")
+	public String getH_entrada() {
+		return h_entrada;
+	}
+
+	public void setH_entrada(String h_entrada) {
+		this.h_entrada = h_entrada;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
 }
